a8b90b79a70efcc95cad1379a83bf42c1ffe6686
evhales@byu.edu <evhales@byu.edu>
2024-11-09 19:04:34 -0700

    updating scores

--- React/src/play/play.jsx
+++ React/src/play/play.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './play.css';
 import Submit from './Submit.jsx';
+import SendScore from './sendScores.jsx';
 
-
-export function Play() {
+export function Play(props) {
   const childRef = useRef();
 
   const disableInputs = () => {
@@ -12,11 +12,10 @@ export function Play() {
   };
 
   const [timerValue, setTimerValue] = useState('');
-  const [finished, setFinished] = useState(false);
 
-    let totalTime = 5;
+  let totalTime = 5;
 
-    const computeTimerValue = () => {
+  const computeTimerValue = () => {
         if(totalTime >= 11) {
             totalTime = totalTime - 1;
             return "00:" + totalTime;
@@ -25,6 +24,7 @@ export function Play() {
             return "00:0" + totalTime;
         } else {
             disableInputs();
+            SendScore(10);
             return "00:00";
         }
     };
@@ -47,7 +47,7 @@ export function Play() {
             <div style={{ height: '200px', overflow: 'auto' }}> 
               <Submit ref={childRef}/>
             </div>
-            <p>Thank you for your submission. Your answers will now be graded and checked by a third party service. </p>
+            <p id='message'></p>
           </div>
       </main>
     </div>
