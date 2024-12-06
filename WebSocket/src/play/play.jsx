d65e49cd947f6aee2894dcbbf3ce2af4ce04c836
evhales@byu.edu <evhales@byu.edu>
2024-12-06 14:36:13 -0700

    trying to debug scores

--- WebSocket/src/play/play.jsx
+++ WebSocket/src/play/play.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './play.css';
 import getWord from './getCategory.jsx';
